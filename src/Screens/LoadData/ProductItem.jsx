bb53c03db31978c08f103c1cd08f2acd17c98067
bhushan-01 <bhushansunilkakade.com>
2023-10-29 11:27:20 +0530

    Final code Release

--- src/Screens/LoadData/ProductItem.jsx
+++ src/Screens/LoadData/ProductItem.jsx
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 16,
+    marginHorizontal:10,
     borderRadius: 12,
     backgroundColor: 'white',
     elevation: 3,
